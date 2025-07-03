e4fb5722730be17a9176b3c4a955d42e150e5267
joSincal <106826998+joSincal@users.noreply.github.com>
2025-07-03 11:47:35 -0600

    create spa

---
+++ 02-pokemon-ssr/src/app/pages/contact/contact-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+    selector: "app-about-page",
+    imports: [],
+    templateUrl: "./contact-page.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ContactPageComponent {}
---
+++ 02-pokemon-ssr/src/app/pages/pricing/pricing-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+    selector: "app-about-page",
+    imports: [],
+    templateUrl: "./pricing-page.component.html",
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PricingPageComponent {}
