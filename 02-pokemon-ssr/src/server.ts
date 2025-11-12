d68033e70a6c4608427f8539d1ba0295def49bc1
joSincal <106826998+joSincal@users.noreply.github.com>
2025-11-12 12:04:22 -0600

    add netlify conf

--- 02-pokemon-ssr/src/server.ts
+++ 02-pokemon-ssr/src/server.ts
@@ -1,16 +1,19 @@
 import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  isMainModule,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express from 'express';
-import { join } from 'node:path';
+    AngularNodeAppEngine,
+    CommonEngine,
+    createNodeRequestHandler,
+    isMainModule,
+    writeResponseToNodeResponse,
+} from "@angular/ssr/node";
+import { render } from "@netlify/angular-runtime/common-engine";
+import express from "express";
+import { join } from "node:path";
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+const browserDistFolder = join(import.meta.dirname, "../browser");
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
+const commonEngine = new CommonEngine();
 
 /**
  * Example Express Rest API endpoints can be defined here.
@@ -28,23 +31,23 @@ const angularApp = new AngularNodeAppEngine();
  * Serve static files from /browser
  */
 app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
+    express.static(browserDistFolder, {
+        maxAge: "1y",
+        index: false,
+        redirect: false,
+    })
 );
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
 app.use((req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+    angularApp
+        .handle(req)
+        .then((response) =>
+            response ? writeResponseToNodeResponse(response, res) : next()
+        )
+        .catch(next);
 });
 
 /**
@@ -52,17 +55,25 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+    const port = process.env["PORT"] || 4000;
+    app.listen(port, (error) => {
+        if (error) {
+            throw error;
+        }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+        console.log(
+            `Node Express server listening on http://localhost:${port}`
+        );
+    });
 }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
+export async function netlifyCommonEngineHandler(
+    request: Request,
+    context: any
+): Promise<Response> {
+    return await render(commonEngine);
+}
