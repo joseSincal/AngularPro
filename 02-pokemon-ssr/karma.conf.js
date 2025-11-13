2bd9ecdfb933a35848cc3dc19fbaf5b7b64f240e
joSincal <106826998+joSincal@users.noreply.github.com>
2025-11-12 19:47:40 -0600

    fix script

--- 02-pokemon-ssr/karma.conf.js
+++ 02-pokemon-ssr/karma.conf.js
@@ -1,14 +1,21 @@
-const { executablePath } = require('puppeteer');
+const { executablePath } = require("puppeteer");
 process.env.CHROME_BIN = executablePath();
 
 module.exports = function (config) {
-  config.set({
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
-      },
-    },
-  });
+    config.set({
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: "ChromeHeadless",
+                flags: [
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--disable-gpu",
+                    "--disable-dev-shm-usage",
+                    "--no-zygote",
+                    "--single-process",
+                ],
+            },
+        },
+        browsers: ["ChromeHeadlessNoSandbox"],
+    });
 };
